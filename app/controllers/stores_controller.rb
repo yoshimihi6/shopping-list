9c914154d8e6c092a9a80547610efe3d92e1a8c3
yoshimihi6 <yoooshimi.36@gmail.com>
2021-12-04 15:48:24 +0900

    店舗名作成機能の実装

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,4 +2,19 @@ class StoresController < ApplicationController
   def new
     @store = Store.new
   end
+
+  def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:store_name, user_ids: [])
+  end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,6 @@
 class Store < ApplicationRecord
   has_many :store_users
   has_many :users, through: :store_users
+
+  validates :store_name, presence: true
 end
