bda5c2444fa08ab1bbf1b2bade6e2029cf35a7f9
yoshimihi6 <yoooshimi.36@gmail.com>
2021-12-04 19:30:41 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
-  root to: "items#index"
+  root to: "stores#index"
   resources :users, only: [:edit, :update]
   resources :stores, only: [:new, :create]
 end
