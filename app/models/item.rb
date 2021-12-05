d6ca8d7337940397620361c35d59e356789d4b07
yoshimihi6 <yoooshimi.36@gmail.com>
2021-12-05 16:00:14 +0900

    商品投稿のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :item_name, presence: true
+  validates :item_name, presence: true, unless: :was_attached?
   validates :price, presence: true
+
+  def was_attached?
+    self.image.attached?
+  end
 end
