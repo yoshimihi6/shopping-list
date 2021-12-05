487cc2b3cff5326a8e1b970f671b263c567906a6
yoshimihi6 <yoooshimi.36@gmail.com>
2021-12-05 15:02:39 +0900

    店舗の削除機能の実装

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,7 @@
 class Store < ApplicationRecord
-  has_many :store_users
+  has_many :store_users, dependent: :destroy
   has_many :users, through: :store_users
-  has_many :items
+  has_many :items, dependent: :destroy
 
   validates :store_name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "stores#index"
   resources :users, only: [:edit, :update]
-  resources :stores, only: [:new, :create] do
+  resources :stores, only: [:new, :create, :destroy] do
     resources :items, only: [:index, :create]
   end
 end
