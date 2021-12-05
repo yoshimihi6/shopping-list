640d7fbe2a926bcf09bc16d708fb6591658b86ec
yoshimihi6 <yoooshimi.36@gmail.com>
2021-12-05 12:16:24 +0900

    メッセージ送信機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @store = Store.find(params[:store_id])
+  end
+
+  def create
+    @store = Store.find(params[:store_id])
+    @item = @store.items.new(item_params)
+    if @item.save
+      redirect_to store_items_path(@store)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :price, :content).merge(user_id: current_user.id)
   end
 end
